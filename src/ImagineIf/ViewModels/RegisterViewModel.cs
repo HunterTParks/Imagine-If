b3c4fc980fc9e8e9c4ebed6cdba5bd949d8014a6
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-12 00:49:40 -0700

    add a User Register view model

---
+++ src/ImagineIf/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImagineIf.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [Compare("Password", ErrorMessage = "The Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
