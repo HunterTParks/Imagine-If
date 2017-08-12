93e8dd3bbb27524c92bcd7516f3fddbcafbb9da6
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-12 01:14:26 -0700

    add Home Controller for routing the homepage

--- src/ImagineIf/Controllers/AccountController.cs
+++ src/ImagineIf/Controllers/AccountController.cs
@@ -32,7 +32,7 @@ namespace ImagineIf.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email  };
+            var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
             IdentityResult result = await _userManager.CreateAsync(user, model.Password);
             if (result.Succeeded)
             {
