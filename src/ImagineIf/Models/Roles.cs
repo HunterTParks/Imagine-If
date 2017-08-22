5f7e64d3e96524b04aa6400846030742f5771369
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 13:08:50 -0700

    remove Roles code because It was buggy

--- src/ImagineIf/Models/Roles.cs
+++ src/ImagineIf/Models/Roles.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using ImagineIf.Models;
+﻿//using System;
+//using System.Threading.Tasks;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using ImagineIf.Models;
 
-private async Task CreateRoles(IServiceProvider serviceProvider)
-{
-    var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-    string[] roleNames = { "Admin", "Moderator", "Member" };
-    IdentityResult roleResult;
+//private async Task CreateRoles(IServiceProvider serviceProvider)
+//{
+//    var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//    var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+//    string[] roleNames = { "Admin", "Moderator", "Member" };
+//    IdentityResult roleResult;
 
-    foreach (var roleName in roleNames)
-    {
-        //creating the roles and seeding them into the database
-        var roleExist = await RoleManager.RoleExistsAsync(roleName);
-        if(!roleExist)
-        {
-            roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
-        }
-    }
+//    foreach (var roleName in roleNames)
+//    {
+//        //creating the roles and seeding them into the database
+//        var roleExist = await RoleManager.RoleExistsAsync(roleName);
+//        if(!roleExist)
+//        {
+//            roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+//        }
+//    }
 
-    //creating a superuser who could maintain the webapp
-    var poweruser = new ApplicationUser
-    {
-        UserName = Configuration.GetSection("UserSettings")["UserEmail"],
-        Email = Configuration.GetSection("UserSettings")["UserEmail"]
-    };
+//    //creating a superuser who could maintain the webapp
+//    var poweruser = new ApplicationUser
+//    {
+//        UserName = Configuration.GetSection("UserSettings")["UserEmail"],
+//        Email = Configuration.GetSection("UserSettings")["UserEmail"]
+//    };
 
-    string UserPassword = Configuration.GetSection("UserSettings")["UserPassword"];
-    var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("UserSettings")["UserEmail"]);
+//    string UserPassword = Configuration.GetSection("UserSettings")["UserPassword"];
+//    var _user = await UserManager.FindByEmailAsync(Configuration.GetSection("UserSettings")["UserEmail"]);
 
-    if(_user == null)
-    {
-        var createPowerUser = await UserManager.CreateAsync(poweruser, UserPassword);
-        if(createPowerUser.Succeeded)
-        {
-            //here we tie the new user to the admin role
-            await UserManager.AddToRoleAsync(poweruser, "Admin");
-        }
-    }
-}
+//    if(_user == null)
+//    {
+//        var createPowerUser = await UserManager.CreateAsync(poweruser, UserPassword);
+//        if(createPowerUser.Succeeded)
+//        {
+//            //here we tie the new user to the admin role
+//            await UserManager.AddToRoleAsync(poweruser, "Admin");
+//        }
+//    }
+//}
