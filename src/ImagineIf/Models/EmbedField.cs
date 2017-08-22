9ef8e703af18c98f4086e6696eac185b19110b59
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 14:11:13 -0700

    add Embed Field Model to our Embed Model Class

---
+++ src/ImagineIf/Models/EmbedField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagineIf.Models
+{
+    public class EmbedField
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public bool Inline { get; set; }
+    }
+}
