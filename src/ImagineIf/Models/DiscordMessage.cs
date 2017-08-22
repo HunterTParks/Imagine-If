7ef1c8bf37791a40c2fbc81a7cd09f89e23ddaf3
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 13:46:25 -0700

    add Discord messages model

---
+++ src/ImagineIf/Models/DiscordMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ImagineIf.Models
+{
+    public class DiscordMessage
+    {
+        public int Id { get; set; }
+        public int Channel_id { get; set; }
+        public ApplicationUser Author { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+        public DateTime Editted_TimeStamp { get; set; }
+        public bool TTS { get; set; }
+        public bool Mention_Everyone { get; set; }
+        public List<ApplicationUser> Mentions { get; set; }
+        public List<Attachment> attachments { get; set; }
+        public List<Embed> Embeds { get; set; }
+        public bool Pinned { get; set; }
+        public int Type { get; set; }
+    }
+}
