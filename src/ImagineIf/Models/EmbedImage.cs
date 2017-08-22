f289af64d2ae78daf6f56f0b00f156d6285b041f
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 14:09:56 -0700

    add Embed Image Model to our Embed Model Class

---
+++ src/ImagineIf/Models/EmbedImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagineIf.Models
+{
+    public class EmbedImage
+    {
+        public string Url { get; set; }
+        public string Proxy_Url { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
