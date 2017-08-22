07db0541b44ad5f0ac1253b85d111d074196d422
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 15:10:53 -0700

    add overwrite Model for our Channel Model class

---
+++ src/ImagineIf/Models/Overwrite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagineIf.Models
+{
+    public class Overwrite
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int Allow { get; set; }
+        public int Deny { get; set; }
+    }
+}
