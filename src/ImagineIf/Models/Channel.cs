e99727ee2e96bebe6abca21530b0d568b188e950
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 15:05:53 -0700

    add more properties to Channel Model class

--- src/ImagineIf/Models/Channel.cs
+++ src/ImagineIf/Models/Channel.cs
@@ -10,7 +10,16 @@ namespace ImagineIf.Models
         public int id { get; set; }
         public int type { get; set; }
         public int guild_id { get; set; }
+        public int Position { get; set; }
+        public List<Overwrite> Permission_Overwrites { get; set; }
         public string name { get; set; }
+        public string Topic { get; set; }
+        public int Bitrate { get; set; }
+        public int User_Limit { get; set; }
+        public List<ApplicationUser> Recipients { get; set; }
+        public string Icon { get; set; }
+        public int Owner_Id { get; set; }
+        public int Application_Id { get; set; }
 
         public static List<DiscordMessage> GetMessages()
         {
