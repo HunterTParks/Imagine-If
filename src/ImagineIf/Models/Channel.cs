df713b93cb6a6a25382ebcf1b50f5ff5e64289b6
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-22 13:38:15 -0700

    add Channel model for Discord integration and remove one line of html in Index

---
+++ src/ImagineIf/Models/Channel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImagineIf.Models
+{
+    public class Channel
+    {
+        public int id { get; set; }
+        public int type { get; set; }
+        public int guild_id { get; set; }
+        public string name { get; set; }
+    }
+}
